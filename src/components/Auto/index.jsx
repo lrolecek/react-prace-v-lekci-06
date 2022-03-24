de3c326c44efddfcf9cbec17a16666f89c2259bc
Luděk Roleček <ludek@rolecek.cz>
2022-03-24 20:35:19 +0100

    akt

--- src/components/Auto/index.jsx
+++ src/components/Auto/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Auto = () => {
 
+	const [tankLevel, setTankLevel] = useState('full');
+
+	const handleClick = () => {
+		setTankLevel('almost empty');
+	}
 
 	return (
 		<div className="auto">
 			<h3>Auto</h3>
+			<p>Stav nádrže: {tankLevel}</p>
+
+			<button onClick={handleClick}>Ujeď hodně kilometrů</button>
 		</div>
 	)
 }
