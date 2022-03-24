8ac5987af81c4b2b3e75453ea48de83c4b2d06f4
Luděk Roleček <ludek@rolecek.cz>
2022-03-24 20:46:04 +0100

    akt

--- src/components/Pocitadlo/index.jsx
+++ src/components/Pocitadlo/index.jsx
@@ -3,10 +3,18 @@ import React, {useState} from 'react';
 
 const Pocitadlo = () => {
 
+	const [count, setCount] = useState('10');
+
+	const handleClick = () => {
+		setCount(count + 1);
+	}
+
 	return (
 		<div className="pocitadlo">
 			<h3>Počítadlo</h3>
+			<p>{count}</p>
 
+			<button onClick={handleClick}>Zvětši</button>
 		</div>
 	)
 }
